245eb51b9b9e23be33059b01c0c61f3a5aa58f4a
Gençay Yıldız <gyildizmail@gmail.com>
2023-02-12 13:59:00 +0300

    Message Broker

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", message);
+}
---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+ISubscriber subscriber = connection.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
