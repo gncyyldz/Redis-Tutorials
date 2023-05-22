4749e6ca68b2c6fc027e39c7ece642d955927ad7
Gençay Yıldız <gyildizmail@gmail.com>
2023-05-22 03:39:42 +0300

    Fix

---
+++ Redis.Sentinel/Controllers/RedisController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Redis.Sentinel.Services;
+
+namespace Redis.Sentinel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        //localhost:4200/api/redis/setvalue/name/gencay
+        [HttpGet("[action]/{key}/{value}")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            var redis = await RedisService.RedisMasterDatabase();
+            await redis.StringSetAsync(key, value);
+            return Ok();
+        }
+        //localhost:4200/api/redis/getvalue/name
+        [HttpGet("[action]/{key}")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            var redis = await RedisService.RedisMasterDatabase();
+            var data = await redis.StringGetAsync(key);
+            return Ok(data.ToString());
+        }
+    }
+}
---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,49 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            EndPoints =
+            {
+                { "localhost", 6383 },
+                { "localhost", 6384 },
+                { "localhost", 6385 }
+            },
+            CommandMap = CommandMap.Sentinel,
+            AbortOnConnectFail = false
+        };
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false
+        };
+        static public async Task<IDatabase> RedisMasterDatabase()
+        {
+            ConnectionMultiplexer sentinelConnection = await ConnectionMultiplexer.SentinelConnectAsync(sentinelOptions);
+
+            System.Net.EndPoint masterEndPoint = null;
+            foreach (System.Net.EndPoint endpoint in sentinelConnection.GetEndPoints())
+            {
+                IServer server = sentinelConnection.GetServer(endpoint);
+                if (!server.IsConnected)
+                    continue;
+                masterEndPoint = await server.SentinelGetMasterAddressByNameAsync("mymaster");
+                break;
+            }
+
+            var localMasterIP = masterEndPoint.ToString() switch
+            {
+                "172.18.0.2:6379" => "localhost:6379",
+                "172.18.0.3:6379" => "localhost:6380",
+                "172.18.0.4:6379" => "localhost:6381",
+                "172.18.0.5:6379" => "localhost:6382",
+            };
+
+            ConnectionMultiplexer masterConnection = await ConnectionMultiplexer.ConnectAsync(localMasterIP);
+            IDatabase database = masterConnection.GetDatabase();
+            return database;
+        }
+    }
+}
